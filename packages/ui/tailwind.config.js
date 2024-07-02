110416fa86d6560152baa5b6114a2f7f7b8528a0
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-02 18:44:07 -0300

    feat(tailwind): extract config to own package

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,8 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+module.exports = require('@repo/tailwind-config/tailwind.config');
