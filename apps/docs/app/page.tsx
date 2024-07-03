abfc8729f871311c8a8237bd32fa43e0a86b3c40
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-03 12:54:32 -0300

    feat(prettier): apply formatting rules

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/image';
-import { Button } from '@repo/ui/button';
-import styles from './page.module.css';
+import Image from 'next/image'
+
+import { Button } from '@repo/ui/button'
+
+import styles from './page.module.css'
 
 export default function Home() {
   return (
@@ -59,7 +61,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/file-text.svg" alt="File icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/file-text.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
           Learn
         </a>
         <a
@@ -67,7 +75,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
           Examples
         </a>
         <a
@@ -75,10 +89,16 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to nextjs.org →
         </a>
       </footer>
     </div>
-  );
+  )
 }
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,6 +1,6 @@
-const { resolve } = require('node:path');
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), 'tsconfig.json');
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /*
  * This is a custom ESLint configuration for use with
@@ -64,4 +64,4 @@ module.exports = {
       },
     ],
   },
-};
+}
