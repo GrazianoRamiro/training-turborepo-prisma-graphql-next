f62920ae1426d549e96dc30db7e2addfcd0327b2
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-03 12:40:56 -0300

    feat(husky): test implementation

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -12,9 +12,9 @@ export function Card({
   return (
     <a
       className={className}
+      target="_blank"
       href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
       rel="noopener noreferrer"
-      target="_blank"
     >
       <h2>
         {title} <span>-&gt;</span>
