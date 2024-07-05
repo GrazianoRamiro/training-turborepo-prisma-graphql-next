88af4aa410331b0d2e5ab1b3577a2bbc9417f343
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-05 14:40:36 -0300

    feat(api): implement nest/config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaService } from './services/prisma.service';
 import { schema } from './graphql/schema';
@@ -7,12 +9,18 @@ import { createContext } from './app.context';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { join } from 'path';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       schema,
       context: createContext,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [join(__dirname, '../../../.env')],
     }),
   ],
   controllers: [AppController],
