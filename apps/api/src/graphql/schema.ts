179cbc5708db77e0d5a4ed6509614880f667517a
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-05 14:51:16 -0300

    fix(api): properly set nexus paths

--- apps/api/src/graphql/schema.ts
+++ apps/api/src/graphql/schema.ts
@@ -6,11 +6,11 @@ import * as types from './types/User';
 export const schema = makeSchema({
   types,
   outputs: {
-    schema: join(__dirname, '../../schema.graphql'),
-    typegen: join(__dirname, '../../nexus-typegen.ts'),
+    schema: join(process.cwd(), 'src/schema.graphql'),
+    typegen: join(process.cwd(), 'src/nexus-typegen.ts'),
   },
   contextType: {
-    module: join(__dirname, '../app.context.ts'),
+    module: join(process.cwd(), 'src/app.context.ts'),
     export: 'Context',
   },
 });
