923425e944bf1d8e68878df14fb00a4bd3cfc83b
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-08 15:27:13 -0300

    feat(api): remove nexus package

--- apps/api/src/graphql/types/User.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'nexus-prisma';
-import { objectType } from 'nexus';
-
-export const UserType = objectType({
-  name: User.$name,
-  definition(t) {
-    t.field(User.id);
-  },
-});
