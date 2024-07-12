b22d4399cd3bba4e1d43ea4de72c0b3b0d8bf429
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-12 15:28:24 -0300

    feat(leagues): add error handling

--- apps/api/src/leagues/leagues.service.ts
+++ apps/api/src/leagues/leagues.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { League } from './leagues.model';
+import { leaguesErrorMessages } from './leagues.constants';
 
 @Injectable()
 export class LeaguesService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   findAll(): Promise<League[]> {
-    return this.databaseService.client.league.findMany();
+    try {
+      return this.databaseService.client.league.findMany();
+    } catch (error) {
+      throw new HttpException(
+        leaguesErrorMessages.LEAGUES_NOT_FOUND,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
