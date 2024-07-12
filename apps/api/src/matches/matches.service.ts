b62770d8943376b136a29f4a850f8d9fb98e5145
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-12 15:28:49 -0300

    feat(matches): add error handling

--- apps/api/src/matches/matches.service.ts
+++ apps/api/src/matches/matches.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { Match } from './matches.model';
+import { matchesErrorMessages } from './matches.constants';
 
 @Injectable()
 export class MatchesService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   findAll(): Promise<Match[]> {
-    return this.databaseService.client.match.findMany();
+    try {
+      return this.databaseService.client.match.findMany();
+    } catch (error) {
+      throw new HttpException(
+        matchesErrorMessages.MATCHES_NOT_FOUND,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
