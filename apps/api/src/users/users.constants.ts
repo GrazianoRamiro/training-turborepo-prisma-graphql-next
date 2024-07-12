1c63e4c9f635ea6bc8353ff1c97644a2b68ac99c
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-07-12 15:29:21 -0300

    feat(users): add error handling

---
+++ apps/api/src/users/users.constants.ts
@@ -0,0 +1,4 @@
+export const usersErrorMessages = {
+  USER_NOT_FOUND: 'USER_NOT_FOUND',
+  USERS_NOT_FOUND: 'USERS_NOT_FOUND',
+};
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from './users.model';
 import { DatabaseService } from 'src/database/database.service';
+import { usersErrorMessages } from './users.constants';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   findAll(): Promise<User[]> {
-    return this.databaseService.client.user.findMany();
+    try {
+      return this.databaseService.client.user.findMany();
+    } catch (error) {
+      throw new HttpException(
+        usersErrorMessages.USERS_NOT_FOUND,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   findOne(id: number): Promise<User> {
-    return this.databaseService.client.user.findUnique({ where: { id } });
+    try {
+      return this.databaseService.client.user.findUnique({ where: { id } });
+    } catch (error) {
+      throw new HttpException(
+        usersErrorMessages.USER_NOT_FOUND,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
