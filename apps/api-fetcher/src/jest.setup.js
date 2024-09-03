f09044790fd5c5f6c04b1fc0ca88f1aee6369076
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-03 17:43:31 -0300

    debug(ci): add logs to identify bug

--- apps/api-fetcher/src/jest.setup.js
+++ apps/api-fetcher/src/jest.setup.js
@@ -8,5 +8,7 @@ const envPath = path.resolve(__dirname, '../../../.env');
 if (fs.existsSync(envPath)) {
   dotenv.config({ path: envPath });
 } else {
+  console.log('Jest URL', process.env.API_FOOTBALL_BASE_URL);
+  console.log('Jest KEY', process.env.API_FOOTBALL_API_KEY);
   console.log('No .env file found, relying on environment variables.');
 }
--- apps/api-fetcher/src/services/api-football.service.ts
+++ apps/api-fetcher/src/services/api-football.service.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
 
+console.log('Initial URL', process.env.API_FOOTBALL_BASE_URL);
+console.log('Initial KEY', process.env.API_FOOTBALL_API_KEY);
+
 class FootballService {
   axiosInstance: AxiosInstance;
 
   constructor() {
-    console.log('CONSTRUCTOR BUILDING HERE');
-    console.log('URL', process.env.API_FOOTBALL_BASE_URL);
-    console.log('KEY', process.env.API_FOOTBALL_API_KEY);
+    console.log('CONSTRUCTOR URL', process.env.API_FOOTBALL_BASE_URL);
+    console.log('CONSTRUCTOR KEY', process.env.API_FOOTBALL_API_KEY);
 
     this.axiosInstance = axios.create({
       baseURL: process.env.API_FOOTBALL_BASE_URL,
