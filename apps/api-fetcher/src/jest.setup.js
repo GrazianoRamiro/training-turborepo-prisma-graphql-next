59dbcc929814b857d6290051c5782ae215c7e8db
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-03 20:33:15 -0300

    debug(ci): test loading env vault

--- apps/api-fetcher/src/jest.setup.js
+++ apps/api-fetcher/src/jest.setup.js
@@ -3,16 +3,15 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
 
-const envPath = path.resolve(__dirname, '../../../.env');
+// const envPath = path.resolve(__dirname, '../../../.env');
 
-if (fs.existsSync(envPath)) {
-  dotenv.config({ path: envPath });
-} else {
-  dotenv.config({
-    debug: true,
-    path: path.resolve(__dirname, '../../../.env.vault'),
-  });
-  console.log('Jest URL', process.env.API_FOOTBALL_BASE_URL);
-  console.log('Jest KEY', process.env.API_FOOTBALL_API_KEY);
-  console.log('No .env file found, relying on environment variables.');
-}
+// if (fs.existsSync(envPath)) {
+//   dotenv.config({ path: envPath });
+// } else {
+dotenv.config({
+  path: path.resolve(__dirname, '../../../.env.vault'),
+});
+console.log('Jest URL', process.env.API_FOOTBALL_BASE_URL);
+console.log('Jest KEY', process.env.API_FOOTBALL_API_KEY);
+console.log('No .env file found, relying on environment variables.');
+// }
--- apps/api-fetcher/src/main.ts
+++ apps/api-fetcher/src/main.ts
@@ -5,16 +5,16 @@ import fs from 'fs';
 
 const envPath = path.resolve(__dirname, '../../../.env');
 
-if (fs.existsSync(envPath)) {
-  dotenv.config({ path: envPath });
-} else {
-  dotenv.config({
-    path: path.resolve(__dirname, '../../../.env.vault'),
-  });
-  console.log('Main URL', process.env.API_FOOTBALL_BASE_URL);
-  console.log('Main KEY', process.env.API_FOOTBALL_API_KEY);
-  console.log('No .env file found, relying on environment variables.');
-}
+// if (fs.existsSync(envPath)) {
+//   dotenv.config({ path: envPath });
+// } else {
+dotenv.config({
+  path: path.resolve(__dirname, '../../../.env.vault'),
+});
+console.log('Main URL', process.env.API_FOOTBALL_BASE_URL);
+console.log('Main KEY', process.env.API_FOOTBALL_API_KEY);
+console.log('No .env file found, relying on environment variables.');
+// }
 
 async function init() {
   const apiResponse = await leaguesService.fetchLeagues(true);
