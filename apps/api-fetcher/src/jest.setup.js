985dbfb65a9a5dbcb54b2fff3c0c6d1b3546a0d1
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-03 19:46:45 -0300

    debug(ci): add more logs

--- apps/api-fetcher/src/jest.setup.js
+++ apps/api-fetcher/src/jest.setup.js
@@ -8,7 +8,9 @@ const envPath = path.resolve(__dirname, '../../../.env');
 if (fs.existsSync(envPath)) {
   dotenv.config({ path: envPath });
 } else {
+  console.log('ENV VARS 1', process.env);
   dotenv.config();
+  console.log('ENV VARS 2', process.env);
   console.log('Jest URL', process.env.API_FOOTBALL_BASE_URL);
   console.log('Jest KEY', process.env.API_FOOTBALL_API_KEY);
   console.log('No .env file found, relying on environment variables.');
