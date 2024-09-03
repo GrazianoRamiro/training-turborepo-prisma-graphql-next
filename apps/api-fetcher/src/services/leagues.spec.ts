039901fb4519a4e43738c2705f08f87b08f908ca
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-03 17:01:28 -0300

    debug(ci): add logs to debug env variables

--- apps/api-fetcher/src/services/leagues.spec.ts
+++ apps/api-fetcher/src/services/leagues.spec.ts
@@ -5,6 +5,8 @@ describe('LeaguesService', () => {
   describe('Integration Tests', () => {
     it('should fetch leagues successfully from the real API', async () => {
       const leagues = await leaguesService.fetchLeagues();
+      console.log('URL', process.env.API_FOOTBALL_BASE_URL);
+      console.log('KEY', process.env.API_FOOTBALL_API_KEY);
       expect(leagues).toBeDefined();
       expect(Array.isArray(leagues)).toBe(true);
     });
