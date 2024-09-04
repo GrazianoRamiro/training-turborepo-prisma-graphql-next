6363fba0e712ccbc45ef51cfaa0c73ac16cbc933
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-03 21:03:37 -0300

    fix(jest): clarify env.vault resolution

--- apps/api-fetcher/src/jest.setup.js
+++ apps/api-fetcher/src/jest.setup.js
@@ -1,18 +1,11 @@
 // jest.setup.js
 const dotenv = require('dotenv');
-const fs = require('fs');
 const path = require('path');
 
-// const envPath = path.resolve(__dirname, '../../../.env');
-
-// if (fs.existsSync(envPath)) {
-//   dotenv.config({ path: envPath });
-// } else {
-console.log('DOTENV_KEY', process.env.DOTENV_KEY);
-dotenv.config({
-  path: path.resolve(__dirname, '../../../.env.vault'),
-});
-console.log('Jest URL', process.env.API_FOOTBALL_BASE_URL);
-console.log('Jest KEY', process.env.API_FOOTBALL_API_KEY);
-console.log('No .env file found, relying on environment variables.');
-// }
+if (process.env.CI) {
+  dotenv.config({
+    path: path.resolve(__dirname, '../../../.env.vault'),
+  });
+} else {
+  dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+}
