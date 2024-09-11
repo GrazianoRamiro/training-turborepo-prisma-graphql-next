c70c092d0b344b4208f85d9f412b99afbb4a3456
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-11 16:16:03 -0300

    feat(ui): include colors in theme

--- packages/ui/src/theme.ts
+++ packages/ui/src/theme.ts
@@ -3,8 +3,17 @@ import { extendTheme } from '@chakra-ui/react'
 export const sportsStatsTheme = extendTheme({
   colors: {
     brand: {
-      100: '#f7c6c7',
-      900: '#1a202c',
+      darkGreenShade1: '#17573d',
+      darkGreenShade2: '#2c5e59',
+      darkGreenShade3: '#015a1c',
+      darkGreenShade4: '#024817',
+      darkGreenShade5: '#102100',
+      lightGreenShade1: '#9acc85',
+      mediumGreenShade1: '#74ad5a',
+      mediumGreenShade2: '#68a54b',
+      vibrantGreen: '#c2e213',
+      dark: '#000',
+      light: '#fff',
     },
   },
 })
