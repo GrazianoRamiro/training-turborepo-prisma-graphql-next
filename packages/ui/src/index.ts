f2f5e8501b4f0074038ababb6c9947b41eff5f91
Ramiro Graziano <graziano.ramiro@gmail.com>
2024-09-11 16:15:39 -0300

    feat(ui): transform index to tsx

--- packages/ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@chakra-ui/react'
-export { Image as NextImage } from '@chakra-ui/next-js'
-export { default as UIProvider } from './providers/chakra.provider'
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export * from '@chakra-ui/react'
+export { Image as NextImage } from '@chakra-ui/next-js'
+export { default as UIProvider } from './providers/chakra.provider'
+
+export { IoHomeSharp as HomeIcon } from 'react-icons/io5'
+export {
+  FaDiceFive as DiceIcon,
+  FaStar as StarIcon,
+  FaRegCalendar as CalendarIcon,
+} from 'react-icons/fa'
+export { MdEmojiEvents as EventIcon, MdPublic as PublicIcon } from 'react-icons/md'
+export { FaFlag as FlagIcon } from 'react-icons/fa6'
+export { HiUsers as UsersIcon } from 'react-icons/hi'
